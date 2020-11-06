273f575ff46baa556c82f9a2fff661cd74afc1f9
elham ahmed alnini <elham307409001@gmail.com>
2020-11-06 23:10:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/criminalintentexam/CrimeListViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.criminalintentexam
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+}
